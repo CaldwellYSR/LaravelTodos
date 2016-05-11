0f1349c6c9ff73f026bbbdd65c8aa3887c26d06b
Matthew Caldwell <caldwellysr@gmail.com>
2016-05-11 08:03:40 -0400

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <title>Laravel Task List</title>
+    </head>
+    <body>
+        <div class='container'>
+            <nav class='navbar navbar-default'>
+            </nav>
+        </div>
+        @yield('content')
+    </body>
+</html>
